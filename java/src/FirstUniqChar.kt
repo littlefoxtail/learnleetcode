44f2d54cff30c56508c4ce7dbc6af6e166094b1a
penglong <penglong@staff.weibo.com>
2018-04-17 14:45:38 +0800

    字符串中的第一个唯一字符

---
+++ java/src/FirstUniqChar.kt
@@ -0,0 +1,34 @@
+import kotlin.Int.Companion
+
+object FirstUniqChar {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    println(firstUniqChar("leetcode"))
+    println(firstUniqChar("loveleetcode"))
+
+  }
+
+  private fun firstUniqChar(s: String): Int {
+    val map = HashMap<Char, Int>()
+    val list = ArrayList<Int>()
+
+    for (i in 0 until s.length) {
+      if (map.containsKey(s[i]))
+        map[s[i]] = Int.MAX_VALUE
+      else
+        map[s[i]] = i
+        list.add(i)
+
+    }
+
+    for (i in 0 until list.size) {
+      if (map[s[list[i]]] != Int.MAX_VALUE) {
+        return list[i]
+      }
+    }
+
+    return -1
+
+  }
+}
