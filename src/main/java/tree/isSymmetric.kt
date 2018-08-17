27adcb9d8cb1422bae63c43289064ddd570deb4d
penglong <penglong@staff.weibo.com>
2018-08-17 10:22:47 +0800

    递归处理对称二叉树的问题

--- src/main/java/tree/isSymmetric.kt
+++ src/main/java/tree/isSymmetric.kt
@@ -1,5 +1,6 @@
 package tree
 
+import com.sun.org.apache.xpath.internal.operations.Bool
 import java.util.*
 
 /**
@@ -26,73 +27,88 @@ import java.util.*
  */
 object isSymmetric {
 
-    @JvmStatic
-    fun main(args: Array<String>) {
-        val root = TreeNode(1)
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val root = TreeNode(1)
 
-        root.left = TreeNode(2)
-        root.right = TreeNode(2)
+    root.left = TreeNode(2)
+    root.right = TreeNode(2)
 
-        root.left?.left = TreeNode(3)
-        root.left?.right = TreeNode(4)
+    root.left?.left = TreeNode(3)
+    root.left?.right = TreeNode(4)
 
-        root.right?.left = TreeNode(4)
-        root.right?.right = TreeNode(3)
-
-
-        println(isSymmetric(root))
+    root.right?.left = TreeNode(4)
+    root.right?.right = TreeNode(3)
 
 
-    }
+    println(isSymmetric2(root))
 
-    /**
-     * 使用BFS对整个二叉树进行层级遍历。在每层中使用Stack判断是否对称。
-     *
-     * 用queue存储每一层的二叉树
-     */
-    fun isSymmetric(root: TreeNode?): Boolean {
+  }
 
-        val queue: Queue<TreeNode?> = LinkedList()
-        val stack: Stack<TreeNode?> = Stack()
-        queue.offer(root)
-        var current: TreeNode?
-        var size: Int
-        var isSymmetric: Boolean
-
-        while(!queue.isEmpty()) {
-            size = queue.size
-            val indices = queue.indices
-            for(i: Int in indices) {
-
-                current = queue.poll()
-                if (current != null) {
-                    queue.offer(current.left)
-                    queue.offer(current.right)
-                }
-                if (size == 1) {
-                    break
-                }
-                if (size % 2 != 0) {
-                    return false
-                }
-
-                if (i < size / 2) {
-                    stack.push(current)
-                } else {
-                    isSymmetric = if(current== null) { current == stack.pop()} else {
-                        current.`val` == stack.pop()?.`val`
-                    }
-                    if (!isSymmetric) {
-                        return false
-                    }
-                }
+  fun isSymmetric2(root: TreeNode?): Boolean {
+    if (root == null)
+      return true
+    return isSymmetric(root.left, root.right)
+  }
 
 
+  fun isSymmetric(left: TreeNode?, right: TreeNode?): Boolean {
+    if (left == null && right == null) return true
+    if (left == null || right == null) return false
+    if (left.`val` == right.`val`)
+      return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left)
+    return false
+  }
 
-            }
+      /**
+       * 使用BFS对整个二叉树进行层级遍历。在每层中使用Stack判断是否对称。
+       *
+       * 用queue存储每一层的二叉树
+       */
+
+  fun isSymmetric(root: TreeNode?): Boolean {
+
+    val queue: Queue<TreeNode?> = LinkedList()
+    val stack: Stack<TreeNode?> = Stack()
+    queue.offer(root)
+    var current: TreeNode?
+    var size: Int
+    var isSymmetric: Boolean
+
+    while (!queue.isEmpty()) {
+      size = queue.size
+      val indices = queue.indices
+      for (i: Int in indices) {
+
+        current = queue.poll()
+        if (current != null) {
+          queue.offer(current.left)
+          queue.offer(current.right)
+        }
+        if (size == 1) {
+          break
+        }
+        if (size % 2 != 0) {
+          return false
         }
 
+        if (i < size / 2) {
+          stack.push(current)
+        } else {
+          isSymmetric = if (current == null) {
+            current == stack.pop()
+          } else {
+            current.`val` == stack.pop()?.`val`
+          }
+          if (!isSymmetric) {
+            return false
+          }
+        }
 
-        return true
+      }
     }
+
+
+    return true
+  }
 }
